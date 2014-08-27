49727e26892b7ad690e8c79623f18b99b6f3c6ad
Claas Koehler <Claas.Koehler@dlr.de>
2014-08-27 09:17:47 +0200

    apply ruby19.diff
    This makes sk_web compatible with Ruby 1.9.
    
    from https://sourceforge.net/p/startkladde/mailman/message/32758790/

--- config/environment.rb
+++ config/environment.rb
@@ -1,6 +1,9 @@
+# encoding: utf-8
+
 require 'fileutils'
 
 # Be sure to restart your server when you modify this file
+
 
 # Specifies gem version of Rails to use when vendor/rails is not present
 #RAILS_GEM_VERSION = '~>2.3.9' unless defined? RAILS_GEM_VERSION
